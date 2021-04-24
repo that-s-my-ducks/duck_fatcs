ccba58c8a5c40e6666c63e070b45625709dbe0c7
Mike Koshakow <mzkosh@gmail.com>
2021-04-24 15:42:27 -0400

    creates barebones server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+const PORT = 8080;
+const server = app.listen(PORT, () =>
+  console.log(`Mixing it up on port ${PORT}`)
+);
