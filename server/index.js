4cd53cd8943b3c23c1dcf3cd859fedeed39d80fc
Mike Koshakow <mzkosh@gmail.com>
2021-04-24 15:58:44 -0400

    adds comments

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
