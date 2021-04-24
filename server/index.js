fe9a20b4b6e28e9e2ef406be89d35dc8d53dd6ca
Mike Koshakow <mzkosh@gmail.com>
2021-04-24 15:58:17 -0400

    serves all files in public dir

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// static serving middleware
+// sends all files in the public dir
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 // sends index.html
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
@@ -14,4 +18,3 @@ const PORT = 8080;
 const server = app.listen(PORT, () =>
   console.log(`Mixing it up on port ${PORT}`)
 );
-
