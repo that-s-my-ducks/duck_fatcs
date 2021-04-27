7f0a377cbc3134c93e89c6074ceb8cd22f2529dd
Mike Koshakow <mzkosh@gmail.com>
2021-04-26 20:25:51 -0400

    sets port from process variable or 8080

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () =>
   console.log(`Mixing it up on port ${PORT}`)
 );
